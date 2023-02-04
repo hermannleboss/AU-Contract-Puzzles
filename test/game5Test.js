5e3a546b9ec7baf0a9310beb0f26502a5eed97b0
Hermann EHO <hermannleboss@gmail.com>
2023-02-04 16:35:30 +0000

    resolve game 5

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,22 +1,37 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const {loadFixture} = require('@nomicfoundation/hardhat-network-helpers');
+const {assert} = require('chai');
+const {Wallet, providers} = require("ethers");
 
 describe('Game5', function () {
-  async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
-    const game = await Game.deploy();
-    let randomWallet = ethers.Wallet.createRandom();
+    async function deployContractAndSetVariables() {
+        const Game = await ethers.getContractFactory('Game5');
+        const game = await Game.deploy();
+        const [signer] = await ethers.getSigners(100);
 
-    return { game , randomWallet};
-  }
-  it('should be a winner', async function () {
-    const { game} = await loadFixture(deployContractAndSetVariables);
-    const lowAddress = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.constants.MaxUint256.toHexString()), 20);
+        return {game, signer};
+    }
 
-    // const lowAddress = ethers.utils.hexZeroPad(ethers.utils.hexlify(ethers.constants.MaxUint256), 20);
-    const lowAddressWallet = new ethers.Wallet(lowAddress);
-    await game.connect(lowAddressWallet).win();
-    console.log("lowAddress", lowAddress)
-    assert(await game.isWon(), 'You did not win the game');
-  });
+    it('should be a winner', async function () {
+        const {game, signer} = await loadFixture(deployContractAndSetVariables);
+        let randomWallet = ethers.Wallet.createRandom();
+        while (randomWallet.address.toString() >= '0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf') {
+            randomWallet = ethers.Wallet.createRandom();
+        }
+        randomWallet = randomWallet.connect(ethers.provider)
+        const tx = {
+            from: signer.getAddress(),
+            to: randomWallet.address,
+            value: ethers.utils.parseEther("1"),
+            nonce: signer.getTransactionCount("latest"),
+            gasLimit: 30000000, // 100000
+            gasPrice: signer.getGasPrice(),
+        }
+
+        await signer.sendTransaction(tx)
+        try {
+            await game.connect(randomWallet).win();
+        } catch (e) {
+        }
+        assert(await game.isWon(), 'You did not win the game');
+    });
 });
