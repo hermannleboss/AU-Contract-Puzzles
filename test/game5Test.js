9d8d31ec882359420e9f2a03f4f9cd5e5abe17a4
Hermann EHO <hermannleboss@gmail.com>
2023-02-04 16:43:15 +0000

    improuve

--- test/game5Test.js
+++ test/game5Test.js
@@ -14,24 +14,18 @@ describe('Game5', function () {
     it('should be a winner', async function () {
         const {game, signer} = await loadFixture(deployContractAndSetVariables);
         let randomWallet = ethers.Wallet.createRandom();
+
         while (randomWallet.address.toString() >= '0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf') {
             randomWallet = ethers.Wallet.createRandom();
         }
         randomWallet = randomWallet.connect(ethers.provider)
         const tx = {
-            from: signer.getAddress(),
             to: randomWallet.address,
             value: ethers.utils.parseEther("1"),
-            nonce: signer.getTransactionCount("latest"),
-            gasLimit: 30000000, // 100000
-            gasPrice: signer.getGasPrice(),
         }
 
         await signer.sendTransaction(tx)
-        try {
-            await game.connect(randomWallet).win();
-        } catch (e) {
-        }
+        await game.connect(randomWallet).win();
         assert(await game.isWon(), 'You did not win the game');
     });
 });
